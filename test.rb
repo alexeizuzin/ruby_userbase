28fe96a5d988dc5f788fa7daed47c2fa6d51fbb4
alexeizuzin <alexeizuzin@ya.ru>
2014-11-26 11:14:36 +0300

    метод добавления контактов одного типа

--- test.rb
+++ test.rb
@@ -1,14 +1,14 @@
 
 # TODO Сделать поддержку нескольких адресов
 class UsersDB
-	private @lang
+	# private @lang # почему-то ошибка выскакивает nil...
 	def initialize(usersArr = [])
 		@usersArr = usersArr
 	end
 	def create(username)
 		if !selectByUsername(username)
 			newUser = {}
-			newUser['username'] = username.to_s
+			newUser['username'] = username
 			newUser['userData'] = {}
 			@usersArr.push(newUser)
 		else
@@ -20,12 +20,22 @@ class UsersDB
 		# TODO переписать поиск оптимальней
 		@usersArr.each{
 			|element|
-			# Зачем делать to_s ?
-			if element['username'].to_s == username.to_s
+			# Зачем делать to_s ? - убрал
+			if element['username'] == username
 			 	user = element
 			end
 		}
 		user
+	end
+	def addContacts(username, type, contactsArr = [])
+		if selectByUsername(username)
+			if !selectByUsername(username)['userData'][type]
+				selectByUsername(username)['userData'][type] = []
+			end
+			selectByUsername(username)['userData'][type] += contactsArr
+		else
+			puts 'This user not exists'
+		end
 	end
 	def selectByIndex(index)
 		@usersArr[index]
@@ -46,21 +56,28 @@ class UsersDB
 end
 
 # Где добавления конкретного адреса, нескольких адресов одного типа (например двух телефонов) ?
-myUsersDB = UsersDB.new([{},{}])
-myUsersDB.create(56)
-myUsersDB.create(55555)
-puts myUsersDB.selectByUsername(55555)
-myUsersDB.updateUsername(55555, {'username' => 'five'})
-myUsersDB.create('55555')
-puts 'kolichestvo polzovateley: ' + myUsersDB.getLength.to_s
-myUsersDB.delete('55555')
-myUsersDB.create('55555')
+myUsersDB = UsersDB.new()
+myUsersDB.create('Pedro')
+puts myUsersDB.selectByUsername('Pedro')
+myUsersDB.addContacts('Pedro', 'email', ['hihihi@ya.ru', 'hohohoho@gmail.com'])
+puts myUsersDB.selectByUsername('Pedro')
+myUsersDB.addContacts('Pedro', 'email', ['haha@mail.ru', 'hehehe@yahoo.com'])
+puts myUsersDB.selectByUsername('Pedro')
 
-puts myUsersDB.selectByIndex(0)
-puts myUsersDB.selectByIndex(1)
-puts myUsersDB.selectByIndex(2)
-puts myUsersDB.selectByIndex(3)
-puts myUsersDB.selectByIndex(4)
-puts myUsersDB.selectByIndex(5)
-puts myUsersDB.selectByUsername('five')
-puts myUsersDB.selectByUsername(56)
+# myUsersDB.create(56)
+# myUsersDB.create(55555)
+# puts myUsersDB.selectByUsername(55555)
+# myUsersDB.updateUsername(55555, {'username' => 'five'})
+# myUsersDB.create('55555')
+# puts 'kolichestvo polzovateley: ' + myUsersDB.getLength.to_s
+# myUsersDB.delete('55555')
+# myUsersDB.create('55555')
+
+# puts myUsersDB.selectByIndex(0)
+# puts myUsersDB.selectByIndex(1)
+# puts myUsersDB.selectByIndex(2)
+# puts myUsersDB.selectByIndex(3)
+# puts myUsersDB.selectByIndex(4)
+# puts myUsersDB.selectByIndex(5)
+# puts myUsersDB.selectByUsername('five')
+# puts myUsersDB.selectByUsername(56)
